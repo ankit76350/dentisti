36ad61635ba0d1fb7172cde40b0f7654ac435ae3
Rohan Shinde <krankit01012003@gmail.com>
2025-04-19 10:04:31 +0530

    building for the admin

--- assets/json/role.js
+++ assets/json/role.js
@@ -1,5 +1,5 @@
-// const role = 'admin'
-// const user = {}
+const role = 'admin'
+const user = {}
 
 // //? For doctor
 // const role = 'doctor'
@@ -17,18 +17,18 @@
 
 
 //? For receptionist
-const role = 'receptionist'
-const user = {
+// const role = 'receptionist'
+// const user = {
     
-        "email": "rohanrecep@gmail.com",
-        "userHospitalId": '1380000000390839', //"hospital_id": "1380000000390839"
-        "isAuthenticated": true,
-        "phone": "7840977390",
-        "userId": "1380000000388086", //"receptionist_id": "1380000000390892" i thing i used it in the billing form
-        "userRole": "receptionist",
-        "username": "Rohan Shinde",
-        // "image" : null
-}
+//         "email": "rohanrecep@gmail.com",
+//         "userHospitalId": '1380000000390839', //"hospital_id": "1380000000390839"
+//         "isAuthenticated": true,
+//         "phone": "7840977390",
+//         "userId": "1380000000388086", //"receptionist_id": "1380000000390892" i thing i used it in the billing form
+//         "userRole": "receptionist",
+//         "username": "Rohan Shinde",
+//         // "image" : null
+// }
 
 
 module.exports = {role , user}
